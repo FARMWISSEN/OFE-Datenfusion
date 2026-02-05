43cc7d87019949a2f174a59217cba6c5045c30a2
Ramm <sebastian.ramm@haw-kiel.de>
2026-02-05 10:59:32 +0100

    helb auf neuen Namen angepasst

--- help/source/conf.py
+++ help/source/conf.py
@@ -40,7 +40,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'IPlugIn'
+project = u'OFE-Datenfusion'
 copyright = u'2013, Lucas Johannsen'
 
 # The version info for the project you're documenting, acts as replacement for
